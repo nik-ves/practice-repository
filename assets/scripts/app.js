f335569f666e7ceefe8b7a3343c74d0d13be7986
nik-ves <nik-ves@hotmail.com>
2021-04-14 18:55:09 +0200

    Converting data types

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+// parseInt(value) is converting the string into a whole number without decimal points if the value of the string is a number
+// parseFloat(value) is doing the same thing as the parseInt just for a decimal numbers
+// parseInt(value) and parseFloat(value) can also be written shortly with a + before a value, example: +userInput.value
+// .toString() is changing the value of something to a string, example: currentResult.toString()
+
 function add() {
-    currentResult = currentResult + userInput.value;
+    currentResult = currentResult + parseInt(userInput.value); 
     outputResult(currentResult, '');
 }
 
