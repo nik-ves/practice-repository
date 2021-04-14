7a80fba3d1f1ed4c3f146715726b2bbcce8cb711
nik-ves <nik-ves@hotmail.com>
2021-04-14 18:45:05 +0200

    More on functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,13 +1,11 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function add(num1, num2) {
-    const result = num1 + num2;
-    return result; // Returns a value
+function add() {
+    currentResult = currentResult + userInput.value;
+    outputResult(currentResult, '');
 }
 
-currentResult = add(1, 2);
-
-let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+addBtn.addEventListener('click', add);
  
-outputResult(currentResult, currentResult);
+
