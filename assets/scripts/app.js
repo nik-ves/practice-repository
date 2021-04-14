df8e80c917a4919e090546e56a36888a62c69d05
nik-ves <nik-ves@hotmail.com>
2021-04-14 19:14:24 +0200

    A simple calculator with JS

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,22 +1,44 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-// parseInt(value) is converting the string into a whole number without decimal points if the value of the string is a number
-// parseFloat(value) is doing the same thing as the parseInt just for a decimal numbers
-// parseInt(value) and parseFloat(value) can also be written shortly with a + before a value, example: +userInput.value
-// .toString() is changing the value of something to a string, example: currentResult.toString()
-
 function getUserNumberInput() {
     return parseInt(userInput.value);
 }
 
-function add() {
-    const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
-
-    currentResult = currentResult + enteredNumber; 
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
     outputResult(currentResult, calcDescription);
 }
 
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initalResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initalResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initalResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initalResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initalResult, enteredNumber);
+}
+
 addBtn.addEventListener('click', add);
- 
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
