49dd7489630fdf8b0b849eaf1cc67cfa307cf056
nik-ves <nik-ves@hotmail.com>
2021-05-02 22:10:57 +0200

    Functions vs Methods

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+function startGame() {
+    console.log('Game is starting...');
+}
+
+const person = {
+    greet: function greet() {
+        console.log('Hello there!')
+    } // Storing a function inside of a object is called METHOD and in the end METHOD is nothing but a function attached to a object
+};
+
+person.greet();
+
+startGameBtn.addEventListener('click', startGame);
