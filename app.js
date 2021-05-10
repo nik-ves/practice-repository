8577bfb3e66c45188e2ca522a2df14bdcb8c52b3
nik-ves <nik-ves@hotmail.com>
2021-05-10 15:31:03 +0200

    Selecting multiple elements & summary

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const h1 = document.querySelector('h1');
-const ul = document.querySelector('ul');
-const li = document.querySelector('.list-item');
+// const h1 = document.querySelector('h1');
+// const ul = document.querySelector('ul');
+// const li = document.querySelector('.list-item');
 
-h1.textContent;
-h1.className;
-h1.textContent = 'Some new text for a header';
-h1.className = 'title';
-h1.style.color = 'red';
-h1.style.backgroundColor = 'green';
+// h1.textContent;
+// h1.className;
+// h1.textContent = 'Some new text for a header';
+// h1.className = 'title';
+// h1.style.color = 'red';
+// h1.style.backgroundColor = 'green';
 
-ul.style.backgroundColor = 'blue';
+// ul.style.backgroundColor = 'blue';
 
-li.style.color = 'red';
+// li.style.color = 'red';
+
+// const input = document.querySelector('input');
+// const h1 = document.getElementById('main-title');
+
+// h1.id = 'new-id';
+
+// input.setAttribute('value', 'Some new text!');
+
+// const listItemElements = document.querySelectorAll('li');
+const listItemElements = document.getElementsByTagName('li');
+
+for (const listItemEl of listItemElements) {
+    console.dir(listItemEl);
+}
