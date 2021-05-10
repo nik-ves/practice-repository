0e83e2c981d85254aff7176df907f3d6f0eee57a
nik-ves <nik-ves@hotmail.com>
2021-05-10 15:46:23 +0200

    Child nodes

--- app.js
+++ app.js
@@ -1,28 +1,7 @@
-// const h1 = document.querySelector('h1');
-// const ul = document.querySelector('ul');
-// const li = document.querySelector('.list-item');
+const ul = document.querySelector('ul');
+ul.children[1] // Selecting the second element
 
-// h1.textContent;
-// h1.className;
-// h1.textContent = 'Some new text for a header';
-// h1.className = 'title';
-// h1.style.color = 'red';
-// h1.style.backgroundColor = 'green';
-
-// ul.style.backgroundColor = 'blue';
-
-// li.style.color = 'red';
-
-// const input = document.querySelector('input');
-// const h1 = document.getElementById('main-title');
-
-// h1.id = 'new-id';
-
-// input.setAttribute('value', 'Some new text!');
-
-// const listItemElements = document.querySelectorAll('li');
-const listItemElements = document.getElementsByTagName('li');
-
-for (const listItemEl of listItemElements) {
-    console.dir(listItemEl);
-}
+ul.firstElementChildl;
+ul.firstChild;
+ul.lastElementChild;
+ul.lastChild;
