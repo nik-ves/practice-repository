50dec5c87690510cdc0be892b2e8635a8f7ba6d1
nik-ves <nik-ves@hotmail.com>
2021-05-21 17:40:54 +0200

    Class work #6

--- app.js
+++ app.js
@@ -107,3 +107,106 @@ console.log('---------- Sledeci primer ----------');
 - problemi specificnih helpera
 (objekti, helperi, refactor, template...)
 */
+
+const timZaProjekte = {
+    clanovi: ['Marko', 'Ana'],
+    imeTima: 'Tim - 1',
+    funkcionalnostTima: function() {
+        return this.clanovi.map(function(clan) {
+            return `${clan} je u timu ${this.imeTima}`;
+        });
+    }
+};
+console.log(timZaProjekte.funkcionalnostTima());
+console.log('----------');
+
+/*
+Resenje problema UNDEFINED
+- koristeci arrow funkciju
+*/
+
+const timZaProjekte1 = {
+    clanovi: ['Marko', 'Ana'],
+    imeTima: 'Tim - 1',
+    funkcionalnostTima: function() {
+        
+        return this.clanovi.map((clan) => {
+            return `${clan} je u timu ${this.imeTima}`;
+        });
+    }
+};
+console.log(timZaProjekte1.funkcionalnostTima());
+console.log('---------- Sledeci primer ----------');
+
+/*
+Sistem inventara
+*/
+
+function aplikacijaInventara(inventar) {
+    return {
+        // pravimo specificne pozive sistema
+        inventar: inventar,
+
+        // prolazimo kroz ceo inventar / ne kroz poziv sistema
+        vrednostInventara: function() {
+            /*
+            - dva argumenta
+            - ukupna vrednost i brendirani racunarski sklop [individualni objekat]
+            - cenu inicijalizujemo na (0)
+            */
+           return this.inventar.reduce((ukupno, racunarskiSklop) =>
+                ukupno + racunarskiSklop.cena, 0);
+        },
+        nadjiNaOsnovuCene: function(naziv) {
+            /*
+            - za specifican naziv brenda (inventara), vracamo tacnu cenu
+            - na osnovu promenljive vrati naziv / reverse lookup
+            */
+           return this.inventar.find(racunarskiSklop => 
+                racunarskiSklop.naziv === naziv).cena;
+        }
+    }
+};
+
+/*
+- Svaki inventar ima svoj naziv i svoju cenu
+- vracamo celokupan inventar, a kao rezultat toga ocekujemo ukupnu cenu / sumu
+*/
+
+const inventar = [
+    {naziv: 'DELL', cena: 750},
+    {naziv: 'Lenovo', cena: 950},
+    {naziv: 'HP', cena: 450},
+    {naziv: 'Asus', cena: 350},
+];
+
+const sisInventara = aplikacijaInventara(inventar);
+console.log(sisInventara.vrednostInventara());
+console.log(sisInventara.nadjiNaOsnovuCene('Asus'));
+console.log('---------- Sledeci primer ----------');
+
+/*
+Novi primer
+*/
+
+function Korisnik(id) {
+    this.id = id;
+}
+
+// generator ID brojeva
+
+function generisiId() {
+    return Math.random() * 21312321123;
+}
+
+/*
+- uzimamo argument korisnika
+- predstavljamo ga kao korisnika
+*/
+
+function napraviKorisnika(korisnik) {
+    korisnik.registrovaniKorisnik = true;
+    return korisnik;
+}
+console.log(napraviKorisnika(new Korisnik(generisiId())));
+console.log('---------- Sledeci primer ----------');
