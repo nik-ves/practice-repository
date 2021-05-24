4c34638e6f8f493f1a2c86f84613e07a62c4b557
nik-ves <nik-ves@hotmail.com>
2021-05-24 17:32:18 +0200

    Class work #10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NewTrainingComponent } from './training/new-training/new-training.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
+import { PreviousTrainingComponent } from './training/previous-training/previous-training.component';
+import { TrainingComponent } from './training/training.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    NewTrainingComponent,
+    CurrentTrainingComponent,
+    PreviousTrainingComponent,
+    TrainingComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
