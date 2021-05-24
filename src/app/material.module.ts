e58c7b44b96671c90bd237202821d4e02df5b5e4
nik-ves <nik-ves@hotmail.com>
2021-05-24 18:11:37 +0200

    Class work #11

---
+++ src/app/material.module.ts
@@ -0,0 +1,16 @@
+/*
+U okviru ovog modula (material.module.ts)
+registrovacemo komponente https://material.angular.io/
+
+- dugme: https://material.angular.io/components/button/overview
+*/
+
+import { NgModule } from "@angular/core";
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule ({
+    imports: [MatButtonModule],
+    exports: [MatButtonModule]
+})
+
+export class MaterialModule {}
