8cbc0537cb72d4c4efe0ae5baa407e70df226919
nik-ves <nik-ves@hotmail.com>
2021-07-07 23:56:09 +0200

    Inserting DOM elements

--- src/app/training/exercise.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Exercise { // Da bi fajl postao public
-    id: string;
-    name: string;
-    duration: number;
-    calories: number;
-    date?: Date; // ? - oznacava da polje nije obavezno
-    status?: 'Zavrsen' | 'Prekinut' | null;
-}
