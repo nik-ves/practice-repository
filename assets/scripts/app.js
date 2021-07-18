6ae875a1f41c7be77a2ac7ae6b6f7a13971fd04c
nik-ves <nik-ves@hotmail.com>
2021-07-18 23:04:10 +0200

    Opening a modal by changing classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,3 +3,9 @@ const addMovieModal = document.getElementById('add-modal');
 
 const startAddMovieButton = document.querySelector('header button');
 // const startAddMovieButton = document.querySelector('header').lastElementChild;
+
+const toggleMovieModal = () => { // function() {}
+    addMovieModal.classList.toggle('visible'); // toggle checks if there is visible class already in the div. If there isnt it will be added, and if there is it will be removed
+};
+
+startAddMovieButton.addEventListener('click', toggleMovieModal);
