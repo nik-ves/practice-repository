a258f09d046ad065ffb3ce76494293c626ca6695
nik-ves <nik-ves@hotmail.com>
2021-07-20 22:15:21 +0200

    Fetching and validating user input

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,6 +5,8 @@ const startAddMovieButton = document.querySelector('header button');
 const backdrop = document.getElementById('backdrop');
 // const backdrop = document.body.firstElementChild; Another way
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
+const userInputs = addMovieModal.querySelectorAll('input'); // Selecting all inputs, in this case we have 3 input fields
 
 const toggleMovieModal = () => { // function() {}
     addMovieModal.classList.toggle('visible'); // toggle checks if there is visible class already in the div. If there isnt it will be added, and if there is it will be removed
@@ -19,10 +21,27 @@ const backdropClickHandler = () => {
     toggleMovieModal();
 };
 
-const cancelAddMovie = () => {
+const cancelAddMovieHandler = () => {
     toggleMovieModal();
 };
 
+const addMovieHandler = () => {
+    const titleValue = userInputs[0].value; // First input at index 0 is title
+    const imageUrlValue = userInputs[1].value; // Image url is index 1
+    const ratingValue = userInputs[2].value; // Rating is index 2
+
+    // trim() - Removes excess white space at the beggining and at the end of the input
+    if (titleValue.trim() === '' || 
+        imageUrlValue.trim() === '' || 
+        ratingValue.trim() === '' || 
+        ratingValue < 1 ||
+        ratingValue > 5) {
+            alert('Please enter valid values (rating between 1 and 5).');
+            return;
+    }
+}
+
 startAddMovieButton.addEventListener('click', toggleMovieModal);
 backdrop.addEventListener('click', backdropClickHandler);
-cancelAddMovieButton.addEventListener('click', toggleMovieModal)
+cancelAddMovieButton.addEventListener('click', toggleMovieModal);
+confirmAddMovieButton.addEventListener('click', addMovieHandler);
