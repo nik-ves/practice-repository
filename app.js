20f7d04860db58764a8f55eca6d2993d79221a6b
nik-ves <nik-ves@hotmail.com>
2021-07-23 22:03:20 +0200

    slice();

--- app.js
+++ app.js
@@ -28,28 +28,35 @@
 
 // console.log(personalData[1]);
 
-const hobbies = ['Cooking', 'Sports'];
+// const hobbies = ['Cooking', 'Sports'];
 
-// Adding elements in an array
-hobbies.push('Reading'); // push adds element in array, and always adds them at the end of an array
-hobbies.unshift('Coding'); // unshift does the opposite of push function, element will be added at the begging of an array
+// // Adding elements in an array
+// hobbies.push('Reading'); // push adds element in array, and always adds them at the end of an array
+// hobbies.unshift('Coding'); // unshift does the opposite of push function, element will be added at the begging of an array
 
-// Removing elements in an array
-hobbies.pop(); // pop function removes last element in array, does not need an argument
-hobbies.shift(); // shift does the opposite of pop function, removes the first element in an array
+// // Removing elements in an array
+// hobbies.pop(); // pop function removes last element in array, does not need an argument
+// hobbies.shift(); // shift does the opposite of pop function, removes the first element in an array
 
-console.log(hobbies);
+// console.log(hobbies);
 
-// Manipulating items in different places in an array
-hobbies[1] = 'Coding';
-// hobbies[5] = 'Reading';
+// // Manipulating items in different places in an array
+// hobbies[1] = 'Coding';
+// // hobbies[5] = 'Reading';
 
-console.log(hobbies);
+// console.log(hobbies);
 
-// Adding with splice
-hobbies.splice(1, 0, 'Good food', 'Baking'); // first arguments is where it should star, second is how many should it delete, and third what to add
-console.log(hobbies);
+// // Adding with splice
+// hobbies.splice(1, 0, 'Good food', 'Baking'); // first arguments is where it should star, second is how many should it delete, and third what to add
+// console.log(hobbies);
 
-// Removing with splice
-hobbies.splice(0, 2); // if you enter only one argument, it will delete all items past that numbers index
-console.log(hobbies);
+// // Removing with splice
+// hobbies.splice(0, 2); // if you enter only one argument, it will delete all items past that numbers index
+// console.log(hobbies);
+
+const testResults = [1, 5.3, 1.5, 10.99, -5, 10];
+const storedResults = testResults.slice(0, 2); // you can specify start and end number of an array for selecting range and excludes them from original array
+
+testResults.push(5.91);
+
+console.log(storedResults, testResults);
