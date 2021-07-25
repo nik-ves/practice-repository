b2ea8c743e7ea3d8acf23af37d6208f14ea070fe
nik-ves <nik-ves@hotmail.com>
2021-07-25 22:46:51 +0200

    forEach(); method

--- app.js
+++ app.js
@@ -54,33 +54,49 @@
 // hobbies.splice(0, 2); // if you enter only one argument, it will delete all items past that numbers index
 // console.log(hobbies);
 
-const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
-// const storedResults = testResults.slice(0, 2); // you can specify start and end number of an array for selecting range and excludes them from original array
-const storedResults = testResults.concat([3.99, 2]); // add elements at the end of an array. it takes an array 
+// const testResults = [1, 5.3, 1.5, 10.99, 1.5, -5, 10];
+// // const storedResults = testResults.slice(0, 2); // you can specify start and end number of an array for selecting range and excludes them from original array
+// const storedResults = testResults.concat([3.99, 2]); // add elements at the end of an array. it takes an array 
 
-testResults.push(5.91);
+// testResults.push(5.91);
 
-console.log(storedResults, testResults);
-console.log(testResults.indexOf(1.5)); 
-// returns index of entered value, it can take 2 values, first is the search value and the second one start index which is optional
-// if there are same values it will only return the index of the first found
-// lastIndexOf() does the same thing only from the opposite direction
+// console.log(storedResults, testResults);
+// console.log(testResults.indexOf(1.5)); 
+// // returns index of entered value, it can take 2 values, first is the search value and the second one start index which is optional
+// // if there are same values it will only return the index of the first found
+// // lastIndexOf() does the same thing only from the opposite direction
 
-const presonData = [{name: 'Nikola'}, {name: 'Test'}];
-console.log(testResults.indexOf({name: 'Test'})); // this will not work, console will return -1 and that indicates that it did not find the searched result
+// const presonData = [{name: 'Nikola'}, {name: 'Test'}];
+// console.log(testResults.indexOf({name: 'Test'})); // this will not work, console will return -1 and that indicates that it did not find the searched result
 
-const test = presonData.find((person, idx, persons) => {
-    return person.name === 'Test';
-}); 
+// const test = presonData.find((person, idx, persons) => {
+//     return person.name === 'Test';
+// }); 
 
-console.log(test);
-// find takes function as an argument and that function takes up 3 arguments
-// first argument will always be single object of that array
-// second one is index of that single element
-// third argument will be the full array
+// console.log(test);
+// // find takes function as an argument and that function takes up 3 arguments
+// // first argument will always be single object of that array
+// // second one is index of that single element
+// // third argument will be the full array
 
-const nikolaIndex = presonData.findIndex((person, idx, persons) => {
-    return person.name === 'Nikola';
-}); 
+// const nikolaIndex = presonData.findIndex((person, idx, persons) => {
+//     return person.name === 'Nikola';
+// }); 
 
-console.log(nikolaIndex);
+// console.log(nikolaIndex);
+
+const prices = [10.99, 5.99, 3.99, 6.59];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+// for (const price of prices) {
+//     taxAdjustedPrices.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+    const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)}
+    taxAdjustedPrices.push(priceObj);
+});
+// takes a function and that function takes up to 3 arguments
+
+console.log(taxAdjustedPrices);
