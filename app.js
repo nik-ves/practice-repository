d56478db46b98a6ddac29f4bdcb7ce8ff3920bfb
nik-ves <nik-ves@hotmail.com>
2021-07-26 23:36:31 +0200

    split(); and join();

--- app.js
+++ app.js
@@ -101,50 +101,59 @@
 
 // console.log(taxAdjustedPrices);
 
-const prices = [10.99, 5.99, 3.99, 6.59];
-const tax = 0.19;
+// const prices = [10.99, 5.99, 3.99, 6.59];
+// const tax = 0.19;
 
-const taxAdjustedPrices = prices.map((price, idx, prices) => {
-    const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)}
-    return priceObj; // map has to return something
-});
-
-// console.log(prices, taxAdjustedPrices);
-
-const sortedPrices = prices.sort((a, b) => {
-    if (a > b) {
-        return 1;
-    } else if (a === b) {
-        return 0;
-    } else {
-        return -1;
-    }
-}); 
-
-console.log(sortedPrices.reverse());
-
-// const filteredArray = prices.filter((price, idx, prices) => { // filters function has to return something
-//     return price > 6;
+// const taxAdjustedPrices = prices.map((price, idx, prices) => {
+//     const priceObj = {index: idx, taxAdjPrice: price * (1 + tax)}
+//     return priceObj; // map has to return something
 // });
 
-const filteredArray = prices.filter(price => price > 6);
+// // console.log(prices, taxAdjustedPrices);
 
-console.log(filteredArray);
+// const sortedPrices = prices.sort((a, b) => {
+//     if (a > b) {
+//         return 1;
+//     } else if (a === b) {
+//         return 0;
+//     } else {
+//         return -1;
+//     }
+// }); 
 
-// let sum = 0;
+// console.log(sortedPrices.reverse());
 
-// prices.forEach((price) => {
-//     sum += price;
-// });
+// // const filteredArray = prices.filter((price, idx, prices) => { // filters function has to return something
+// //     return price > 6;
+// // });
 
-// for (const price of prices) {
-//     sum += price;
-// };
+// const filteredArray = prices.filter(price => price > 6);
+
+// console.log(filteredArray);
+
+// // let sum = 0;
+
+// // prices.forEach((price) => {
+// //     sum += price;
+// // });
+
+// // for (const price of prices) {
+// //     sum += price;
+// // };
+
+// // console.log(sum);
+
+// const sum = prices.reduce((prevValue, curValue, curIndex, prices) => {
+//     return prevValue + curValue;
+// }, 0);
 
 // console.log(sum);
 
-const sum = prices.reduce((prevValue, curValue, curIndex, prices) => {
-    return prevValue + curValue;
-}, 0);
+const data = 'new york;10.99;2000';
 
-console.log(sum);
+const transformedData = data.split(';'); // argument is a seperator 
+console.log(transformedData);
+
+const nameFragments = ['Nikola', 'Veselinovic'];
+const name = nameFragments.join(' '); // argument is a seperator
+console.log(name);
