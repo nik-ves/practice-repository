41c241c6d2f0d4372166688e10bdaf390b58d149
nik-ves <nik-ves@hotmail.com>
2021-08-12 17:46:26 +0200

    Validation and resetting logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,27 +9,42 @@ const AddUser = props => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
 
+  const resetUserInputs = () => {
+    setEnteredUsername(''); // Reseting form username input
+    setEnteredAge(''); // Reseting form age input
+  };
+
   const addUserHandler = (event) => {
     event.preventDefault(); // preventing reloading a page when submiting
-    console.log(enteredUsername, enteredAge);
+
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) { // Checks if entered field is empty field
+      return; // return finish the function
+    }
+
+    if (+enteredAge < 1) { // plus converts enteredAge to number since its a string
+      return;
+    }
+
+    console.log(enteredUsername, enteredAge); // Loggin user input
+    resetUserInputs();
   };
 
   const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
+    setEnteredUsername(event.target.value); // Getting username value 
   };
 
   const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    setEnteredAge(event.target.value); // Getting age value
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" onChange={usernameChangeHandler} /> 
+        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} /> 
 
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" onChange={ageChangeHandler} /> 
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} /> 
 
         <Button type="submit">Add User</Button>
       </form>
