ca9a05800fdb3a54747498e66c9277be41886f56
nik-ves <nik-ves@hotmail.com>
2021-09-11 12:11:33 +0200

    Starter files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import React, { useState } from "react";
+import Counter from './components/Counter';
 
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false); // default value for cartIsShown is FALSE
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
-
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
-      <main>
-        <Meals />
-      </main>
-    </CartProvider>
+    <Counter />
   );
 }
 
