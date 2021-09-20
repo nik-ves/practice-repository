2f7c1ea1d92feb835a5d48315fa8710f3ee5ce22
nik-ves <nik-ves@hotmail.com>
2021-09-20 22:14:33 +0200

    Adding dynamic routes

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,15 @@
 const Products = () => {
-  return <h1>Products Page</h1>;
+  return (
+    <section>
+      <h1>Products Page</h1>
+
+      <ul>
+        <li>A Book</li>
+        <li>A Carpet</li>
+        <li>An Online Course</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
