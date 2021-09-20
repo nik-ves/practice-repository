eb540f10d5ee7333bf325220b917c2300f430f29
nik-ves <nik-ves@hotmail.com>
2021-09-20 22:36:36 +0200

    Redirecting the user

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { Route, Switch } from "react-router-dom"; // switch makes sure that only one of the routes will be active at the time
+import { Route, Switch, Redirect } from "react-router-dom"; // switch makes sure that only one of the routes will be active at the time
 
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import ProductDetail from "./pages/ProductDetail";
 import MainHeader from "./components/MainHeader";
+
+// setting path as "/" and adding exact will setup our-domain.com as a welcome/inital page
 
 function App() {
   return (
@@ -11,7 +13,10 @@ function App() {
       <MainHeader />
       <main>
         <Switch>
-          <Route path="/welcome">
+          <Route path="/" exact>
+            <Redirect to="/welcome" />
+          </Route>
+          <Route path="/welcome"> 
             <Welcome />
           </Route>
           <Route path="/products" exact>
