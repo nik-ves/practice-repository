14c2e78d797d031f527e72267e864b47fe6641d3
nik-ves <nik-ves@hotmail.com>
2021-09-21 21:08:23 +0200

    Route and Switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllQuotes from "./components/pages/AllQuotes";
+import QuoteDetail from "./components/pages/QuoteDetail";
+import NewQuote from "./components/pages/NewQuote";
+
 function App() {
   return (
-    <div>
-
-    </div>
+    <Switch>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-qoute">
+        <NewQuote />
+      </Route>
+    </Switch>
   );
 }
 
