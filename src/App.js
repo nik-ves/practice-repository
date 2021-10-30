18e26e45fb2f36cd91a1a7fc0b22730c1c692a05
nik-ves <nik-ves@hotmail.com>
2021-10-30 02:16:36 +0200

    New starter project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import { Switch, Route } from 'react-router-dom';
+import React from 'react';
 
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import Ingredients from './components/Ingredients/Ingredients';
 
-function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
-  );
-}
+const App = props => {
+  return <Ingredients />;
+};
 
 export default App;
