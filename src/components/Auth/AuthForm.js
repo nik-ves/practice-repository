1eb285f85333e0dae9f16bef675c2487a580ae66
nik-ves <nik-ves@hotmail.com>
2021-11-03 17:21:40 +0100

    Keeping the user logged in after refreshing the page

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,8 +1,8 @@
-import { useState, useRef, useContext } from 'react';
-import { useHistory } from 'react-router';
+import { useState, useRef, useContext } from "react";
+import { useHistory } from "react-router";
 
-import AuthContext from '../../context/auth-context';
-import classes from './AuthForm.module.css';
+import AuthContext from "../../context/auth-context";
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const history = useHistory();
@@ -28,21 +28,21 @@ const AuthForm = () => {
     let url;
     if (isLogin) {
       url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB9s0_oW-R_TZGN0GN-vSrsHvIvpZ-RQ4A';
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB9s0_oW-R_TZGN0GN-vSrsHvIvpZ-RQ4A";
     } else {
       url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9s0_oW-R_TZGN0GN-vSrsHvIvpZ-RQ4A';
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9s0_oW-R_TZGN0GN-vSrsHvIvpZ-RQ4A";
     }
 
     fetch(url, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
         email: enteredEmail,
         password: enteredPassword,
         returnSecureToken: true,
       }),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
       .then((res) => {
@@ -51,7 +51,7 @@ const AuthForm = () => {
           return res.json();
         } else {
           return res.json().then((data) => {
-            let errorMessage = 'Authentication failed!';
+            let errorMessage = "Authentication failed!";
             // if (data && data.error && data.error.message) {
             //   errorMessage = data.error.message;
             // }
@@ -62,7 +62,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         authContext.login(data.idToken);
-        history.replace('/')
+        history.replace("/");
       })
       .catch((err) => {
         alert(err.message);
@@ -71,32 +71,32 @@ const AuthForm = () => {
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
       <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required ref={emailInputRef} />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
+          <label htmlFor="password">Your Password</label>
           <input
-            type='password'
-            id='password'
+            type="password"
+            id="password"
             required
             ref={passwordInputRef}
           />
         </div>
         <div className={classes.actions}>
           {!isLoading && (
-            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+            <button>{isLogin ? "Login" : "Create Account"}</button>
           )}
           {isLoading && <p>Sending request...</p>}
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
--- src/context/auth-context.js
+++ src/context/auth-context.js
@@ -8,16 +8,19 @@ const AuthContext = React.createContext({
 });
 
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem('token');
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token; // !! converts this value to true or false boolean
 
   const loginHandler = (token) => {
     setToken(token);
+    localStorage.setItem("token", token);
   };
 
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
   };
 
   const contextValue = {
