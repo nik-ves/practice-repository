58d32d78541eaa3d717418db235186a354ff9cef
nik-ves <nik-ves@hotmail.com>
2021-11-04 14:21:09 +0100

    Working with arrays and object types

--- basics.ts
+++ basics.ts
@@ -8,3 +8,24 @@ let age: number = 3;
 let userName: string = "Resi";
 
 let isInstructor: boolean = true;
+
+// More complex types
+
+let hobbies: string[]; // tells typescript that we wnat to have an array of strings, same can be done for number[] and boolean[]
+
+hobbies = ["Sports", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+}; // not creating an object but defining the type which is an object
+
+person = {
+  name: "Nikola",
+  age: 23,
+};
+
+let people: { // here we are saying that we want to store na array of defined object
+  name: string;
+  age: number;
+}[];
