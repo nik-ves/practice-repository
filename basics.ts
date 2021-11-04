b02b6966f1890a5ada57d4c544597500ba9e655b
nik-ves <nik-ves@hotmail.com>
2021-11-04 14:29:17 +0100

    Understanding type inference, union types and type aliases

--- basics.ts
+++ basics.ts
@@ -10,7 +10,6 @@ let userName: string = "Resi";
 let isInstructor: boolean = true;
 
 // More complex types
-
 let hobbies: string[]; // tells typescript that we wnat to have an array of strings, same can be done for number[] and boolean[]
 
 hobbies = ["Sports", "Cooking"];
@@ -29,3 +28,12 @@ let people: { // here we are saying that we want to store na array of defined ob
   name: string;
   age: number;
 }[];
+
+// Type inference
+let course = 'React - Complete Guide';
+
+// course = 1234; // typescript recognized in the step above that the course is type string
+// defining a type should be avoided because its redundant 
+
+let newCourse: string | number = 'React - Complete Guide';
+newCourse = 1234;
