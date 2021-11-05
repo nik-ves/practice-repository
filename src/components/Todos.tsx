872f1f0d74b0d9c0dc3663d30a9c5f8ace26e6c0
nik-ves <nik-ves@hotmail.com>
2021-11-05 17:53:52 +0100

    Practice with typescript props

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   // React.FC makes it clear that code is a function that acts as a function component
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
