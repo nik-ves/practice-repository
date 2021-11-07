a6a509f6e99a006cb90fecca5189cf97dbdb0663
nik-ves <nik-ves@hotmail.com>
2021-11-07 18:20:01 +0100

    Working with function props

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  const todos = [
-    new Todo('Learn react'),
-    new Todo('Learn typescript')
-  ]
+  const todos = [new Todo("Learn react"), new Todo("Learn typescript")];
 
-  return <Todos items={todos} />;
+  const addTodoHandler = (todoText: string) => {};
+
+  return (
+    <div>
+      <NewTodo onAddTodo={addTodoHandler} />
+      <Todos items={todos} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,6 +1,6 @@
 import { useRef } from "react";
 
-const NewTodo = () => {
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
   const todoTextInputRef = useRef<HTMLInputElement>(null); // must have a generic type and a start value for a ref to work
 
   const submitHandler = (event: React.FormEvent) => {
@@ -8,6 +8,13 @@ const NewTodo = () => {
     event.preventDefault();
 
     const enteredText = todoTextInputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      // throw an error
+      return;
+    }
+
+    props.onAddTodo(enteredText);
   };
 
   return (
