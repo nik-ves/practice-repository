74c5de96383879c7c85dbdeb00d5473b21bda014
nik-ves <nik-ves@hotmail.com>
2021-11-07 18:44:17 +0100

    Removing a todo

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -2,12 +2,18 @@ import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
 import classes from "./Todos.module.css";
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   // React.FC makes it clear that code is a function that acts as a function component
   return (
     <ul className={classes.todos}>
       {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
