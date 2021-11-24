771b9810f344d297c76a1349afb3ffdf1550534e
nik-ves <nik-ves@hotmail.com>
2021-11-24 12:56:03 +0100

    Refactoring some code

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 "use strict";
 
-let secretNumberBox = document.querySelector(".number");
-let message = document.querySelector(".message");
 let scoreBox = document.querySelector(".score");
 let highscoreBox = document.querySelector(".highscore");
-let body = document.querySelector("body");
 
 const buttonCheck = document.querySelector(".check");
 const buttonAgain = document.querySelector(".again");
@@ -15,44 +12,45 @@ console.log(secretNumber);
 let score = 20;
 let highscore = 0;
 
+const displayMessageHandler = (message) => {
+  document.querySelector(".message").textContent = message;
+};
+
+const secretNumberHandler = (secretNumber, width) => {
+  let secretNumberBox = document.querySelector(".number");
+
+  secretNumberBox.style.width = width;
+  secretNumberBox.textContent = secretNumber;
+}
+
+const backgroundColorHandler = (color) => {
+  document.querySelector("body").style.backgroundColor = color;
+}
+
 buttonCheck.addEventListener("click", () => {
   const guess = Number(document.querySelector(".guess").value);
 
   // no input check
   if (!guess) {
-    message.textContent = "No number";
+    displayMessageHandler('No number!');
 
     // quess is correct
   } else if (guess === secretNumber) {
-    message.textContent = "Correct number!";
-    body.style.backgroundColor = "#60b347";
-    secretNumberBox.style.width = "30rem";
-    secretNumberBox.textContent = secretNumber;
+    displayMessageHandler("Correct number!");
+    backgroundColorHandler("#60b347");
+    secretNumberHandler(secretNumber, "30rem");
 
     if (score > highscore) {
       highscore = score;
       highscoreBox.textContent = highscore;
     }
-
-    // guess is low
-  } else if (guess < secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      message.textContent = "Too low!";
+      displayMessageHandler(guess > secretNumber ? "Too high!" : "Too low!")
       score--;
       scoreBox.textContent = score;
     } else {
-      message.textContent = "You lost the game!";
-      scoreBox.textContent = 0;
-    }
-
-    // guess is high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      message.textContent = "Too high!";
-      score--;
-      scoreBox.textContent = score;
-    } else {
-      message.textContent = "You lost the game!";
+      displayMessageHandler("You lost the game!");
       scoreBox.textContent = 0;
     }
   }
@@ -64,9 +62,8 @@ buttonAgain.addEventListener("click", () => {
   console.log(secretNumber);
 
   scoreBox.textContent = score;
-  secretNumberBox.style.width = "15rem";
-  secretNumberBox.textContent = "?";
-  body.style.backgroundColor = "#222";
-  message.textContent = "Start guessing...";
+  secretNumberHandler("?", "15rem");
+  backgroundColorHandler("#222");
+  displayMessageHandler("Start guessing...");
   document.querySelector(".guess").value = "";
 });
