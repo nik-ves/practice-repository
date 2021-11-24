2b9e0d92ddd4e6fa76c60b30b93580cefec59349
nik-ves <nik-ves@hotmail.com>
2021-11-24 13:29:23 +0100

    Working with classes

--- script.js
+++ script.js
@@ -1,69 +1,23 @@
 "use strict";
 
-let scoreBox = document.querySelector(".score");
-let highscoreBox = document.querySelector(".highscore");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
 
-const buttonCheck = document.querySelector(".check");
-const buttonAgain = document.querySelector(".again");
-
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-console.log(secretNumber);
-
-let score = 20;
-let highscore = 0;
-
-const displayMessageHandler = (message) => {
-  document.querySelector(".message").textContent = message;
+const openModalHandler = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 };
 
-const secretNumberHandler = (secretNumber, width) => {
-  let secretNumberBox = document.querySelector(".number");
-
-  secretNumberBox.style.width = width;
-  secretNumberBox.textContent = secretNumber;
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", openModalHandler);
 }
 
-const backgroundColorHandler = (color) => {
-  document.querySelector("body").style.backgroundColor = color;
-}
+const closeModalHandler = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
-buttonCheck.addEventListener("click", () => {
-  const guess = Number(document.querySelector(".guess").value);
-
-  // no input check
-  if (!guess) {
-    displayMessageHandler('No number!');
-
-    // quess is correct
-  } else if (guess === secretNumber) {
-    displayMessageHandler("Correct number!");
-    backgroundColorHandler("#60b347");
-    secretNumberHandler(secretNumber, "30rem");
-
-    if (score > highscore) {
-      highscore = score;
-      highscoreBox.textContent = highscore;
-    }
-  } else if (guess !== secretNumber) {
-    if (score > 1) {
-      displayMessageHandler(guess > secretNumber ? "Too high!" : "Too low!")
-      score--;
-      scoreBox.textContent = score;
-    } else {
-      displayMessageHandler("You lost the game!");
-      scoreBox.textContent = 0;
-    }
-  }
-});
-
-buttonAgain.addEventListener("click", () => {
-  score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
-  console.log(secretNumber);
-
-  scoreBox.textContent = score;
-  secretNumberHandler("?", "15rem");
-  backgroundColorHandler("#222");
-  displayMessageHandler("Start guessing...");
-  document.querySelector(".guess").value = "";
-});
+btnCloseModal.addEventListener("click", closeModalHandler);
+overlay.addEventListener("click", closeModalHandler);
