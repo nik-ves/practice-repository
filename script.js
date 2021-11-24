94567560c133294970612c089f25b86c25a610a4
nik-ves <nik-ves@hotmail.com>
2021-11-24 11:30:34 +0100

    Handling click events

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 "use strict";
 
-let message = (document.querySelector(".message").textContent = "test");
-let number = document.querySelector(".number");
-let score = document.querySelector(".score");
-let inputField = document.querySelector(".guess").value = 23;
+// let message = (document.querySelector(".message").textContent = "test");
+// let number = document.querySelector(".number");
+// let score = document.querySelector(".score");
+// let inputField = document.querySelector(".guess").value = 23;
 
-console.log(inputField)
+// console.log(inputField)
+
+const button = document.querySelector(".check");
+
+button.addEventListener("click", () => {
+  const inputField = Number(document.querySelector(".guess").value);
+  const message = document.querySelector(".message");
+
+  console.log(inputField, typeof inputField);
+
+  // checking if there is no input
+  if (!inputField) {
+    document.querySelector(".message").textContent = "No number";
+  }
+});
