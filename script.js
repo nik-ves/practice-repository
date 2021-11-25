54a01aaa24571f9b117ad9f45a98c9d90225567e
nik-ves <nik-ves@hotmail.com>
2021-11-25 22:37:48 +0100

    Scoping in practice

--- script.js
+++ script.js
@@ -1,92 +1,33 @@
 "use strict";
+"use strict";
 
-// elements
-const player0El = document.querySelector(".player--0");
-const player1El = document.querySelector(".player--1");
-const score0El = document.querySelector("#score--0");
-const score1El = document.querySelector("#score--1");
-const current0El = document.querySelector("#current--0");
-const current1El = document.querySelector("#current--1");
-const diceEl = document.querySelector(".dice");
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
 
-// buttons
-const newBtn = document.querySelector(".btn--new");
-const rollBtn = document.querySelector(".btn--roll");
-const holdBtn = document.querySelector(".btn--hold");
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}.`;
+    console.log(output);
 
-// scores
-let scores, currentScore, activePlayer, playing;
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = "Test";
+      const type = `Oh, and you're a millenial, ${firstName}.`;
+      console.log(type);
 
-// helper functions
-const initState = () => {
-  scores = [0, 0];
-  currentScore = 0;
-  activePlayer = 0;
-  playing = true;
-
-  score0El.textContent = 0;
-  score1El.textContent = 0;
-  current0El.textContent = 0;
-  current1El.textContent = 0;
-
-  diceEl.classList.add("hidden");
-  player0El.classList.remove("player--winner");
-  player1El.classList.remove("player--winner");
-  player0El.classList.add("player--active");
-  player1El.classList.remove("player--active");
-};
-
-initState();
-
-const switchPlayerHandler = () => {
-  document.querySelector(`#current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-
-  player0El.classList.toggle("player--active");
-  player1El.classList.toggle("player--active");
-};
-
-// rolling dice functionality
-rollBtn.addEventListener("click", () => {
-  if (playing) {
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    diceEl.classList.remove("hidden");
-    diceEl.src = `dice-${dice}.png`;
-
-    if (dice !== 1) {
-      currentScore += dice;
-
-      document.querySelector(`#current--${activePlayer}`).textContent =
-        currentScore;
-    } else {
-      switchPlayerHandler();
+      function add(a, b) {
+        return a + b;
+      }
     }
+    // console.log(type);
+    console.log(millenial);
+
+    // add(2, 3);
   }
-});
 
-// hold score functionality
-holdBtn.addEventListener("click", () => {
-  if (playing) {
-    scores[activePlayer] += currentScore;
+  printAge();
 
-    document.querySelector(`#score--${activePlayer}`).textContent =
-      scores[activePlayer];
+  return age;
+}
 
-    if (scores[activePlayer] >= 100) {
-      playing = false;
-      diceEl.classList.add("hidden");
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add("player--winner");
-      document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.remove("player--active");
-    } else {
-      switchPlayerHandler();
-    }
-  }
-});
-
-// reset game functionality
-newBtn.addEventListener("click", initState);
+const firstName = "Nikola";
+calcAge(1990);
