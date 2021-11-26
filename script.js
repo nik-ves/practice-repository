4f548219cce0d64cff28770ea9929ee056fe4b39
nik-ves <nik-ves@hotmail.com>
2021-11-26 10:16:25 +0100

    Hoisting and TDZ in practice

--- script.js
+++ script.js
@@ -1,33 +1,72 @@
 "use strict";
-"use strict";
 
-function calcAge(birthYear) {
-  const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
 
-  function printAge() {
-    const output = `${firstName}, you are ${age}, born in ${birthYear}.`;
-    console.log(output);
+//   function printAge() {
+//     const output = `${firstName}, you are ${age}, born in ${birthYear}.`;
+//     console.log(output);
 
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      const firstName = "Test";
-      const type = `Oh, and you're a millenial, ${firstName}.`;
-      console.log(type);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const firstName = "Test";
+//       const type = `Oh, and you're a millenial, ${firstName}.`;
+//       console.log(type);
 
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    // console.log(type);
-    console.log(millenial);
+//       function add(a, b) {
+//         return a + b;
+//       }
+//     }
+//     // console.log(type);
+//     console.log(millenial);
 
-    // add(2, 3);
-  }
+//     // add(2, 3);
+//   }
 
-  printAge();
+//   printAge();
 
-  return age;
+//   return age;
+// }
+
+// const firstName = "Nikola";
+// calcAge(1990);
+
+// hoisting with variables
+
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = "Nikola";
+let job = "Student";
+const year = 1998;
+
+// functions
+
+// console.log(addDecal(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecal(a, b) {
+  return a + b;
 }
 
-const firstName = "Nikola";
-calcAge(1990);
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  // console.log("all products are deleted");
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
