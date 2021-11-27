643a588569babf74b0b9ad2d2927cf8fea6e54e5
nik-ves <nik-ves@hotmail.com>
2021-11-27 11:32:20 +0100

    Optional chaining ( ?. )

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 const flights =
   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
 // Data needed for first part of the section
 const openingHours = {
@@ -16,7 +16,7 @@ const openingHours = {
     open: 11,
     close: 23,
   },
-  [`day-${2+4}`]: {
+  [weekdays[5]]: {
     open: 0, // Open 24 hours
     close: 24,
   },
@@ -52,6 +52,37 @@ const restaurant = {
     console.log(mainIngredient, otherIngredients);
   },
 };
+
+if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+// console.log(restaurant.openingHours.mon.open);
+
+// with optional chaining
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+// example
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+for (const day of days) {
+  console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? "closed";
+  console.log(`On ${day}, we open at ${open}.`);
+}
+
+// methods
+console.log(restaurant.order?.(0, 1) ?? "method does not exist");
+console.log(restaurant.orderRisotto?.(0, 1) ?? "method does not exist");
+
+// arrays
+const users = [
+  {
+    name: "Nikola",
+    email: "hello@nikola.com",
+  },
+];
+
+console.log(users[0]?.name ?? "user array empty");
 
 // const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
