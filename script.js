9abcd2c1cfb37b454a13ea971ce3e4145aadb0c3
nik-ves <nik-ves@hotmail.com>
2021-11-28 21:31:06 +0100

    Maps: Iteration

--- script.js
+++ script.js
@@ -53,36 +53,80 @@ const restaurant = {
   },
 };
 
-const rest = new Map();
-rest.set("name", "Classico Italiano");
-rest.set(1, "Firenze, Italy");
-rest.set(2, "Lisbon, Portugal");
+// const rest = new Map();
+// rest.set("name", "Classico Italiano");
+// rest.set(1, "Firenze, Italy");
+// rest.set(2, "Lisbon, Portugal");
 
-rest
-  .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
-  .set("open", 11)
-  .set("close", 23)
-  .set(true, "We are open")
-  .set(false, "We are closed");
+// rest
+//   .set("categories", ["Italian", "Pizzeria", "Vegetarian", "Organic"])
+//   .set("open", 11)
+//   .set("close", 23)
+//   .set(true, "We are open")
+//   .set(false, "We are closed");
 
-console.log(rest.get("name"));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// console.log(rest.get("name"));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
 
-const time = 21;
-console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
+// const time = 21;
+// console.log(rest.get(time > rest.get("open") && time < rest.get("close")));
 
-console.log(rest.has("categories"));
-rest.delete(2);
-console.log(rest.size);
-// rest.clear() // removes all elements
+// console.log(rest.has("categories"));
+// rest.delete(2);
+// console.log(rest.size);
+// // rest.clear() // removes all elements
 
-rest.set([1, 2], "Test");
-rest.set(document.querySelector('h1'), 'Heading')
+// rest.set([1, 2], "Test");
+// rest.set(document.querySelector("h1"), "Heading");
 
-console.log(rest);
+// console.log(rest);
 
-console.log(rest.get([1, 2]));
+// console.log(rest.get([1, 2]));
+
+// const question = new Map([
+//   ["question", "What is the best programming language in the world?"],
+//   [1, "C"],
+//   [2, "Java"],
+//   [3, "JavaScript"],
+//   ["correct", 3],
+//   [true, "Correct!"],
+//   [false, "Wrong answer!"],
+// ]);
+
+// console.log(question);
+
+// // convert object to map
+// console.log(Object.entries(openingHours));
+
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// // Quizz
+// console.log(question.get("question"));
+
+// for (const [key, value] of question) {
+//   if (typeof key === "number") {
+//     console.log(`Answer ${key}: ${value}`);
+//   }
+// }
+
+// const answer = Number(prompt("Your answer:"));
+// console.log(answer);
+
+// console.log(question.get("correct"));
+
+// console.log(question.get(answer === question.get("correct")));
+
+// // convert map to an array
+// console.log([...question]);
+// console.log(question);
+
+// if (answer === question.get('correct')) {
+//   console.log(question.get(true))
+// } else {
+//   console.log(question.get(false))
+// }
 
 // const ordersSet = new Set([
 //   "Pasta",
