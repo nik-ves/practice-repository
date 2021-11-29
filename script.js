95b16cbe6e263ca221f53d3752c6a257a9cc9037
nik-ves <nik-ves@hotmail.com>
2021-11-29 20:13:13 +0100

    Value vs reference

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+/* 
 const bookings = [];
 
 const createBooking = function (
@@ -27,3 +28,32 @@ createBooking("LH123", 2);
 createBooking("LH123", 5);
 
 createBooking("LH123", undefined, 1000);
+*/
+
+const flight = "LH234";
+const nikola = {
+  name: "Nikola Veselinovic",
+  passport: 22131241250,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 22131241250) {
+    alert("Check in ");
+  } else {
+    alert("Wrong passport");
+  }
+};
+
+checkIn(flight, nikola);
+console.log(flight);
+console.log(nikola);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random * 100000000);
+};
+
+newPassport(nikola);
+checkIn(flight, nikola);
