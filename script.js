df63c261b758fc11f8d76123827473fe3c7b41be
nik-ves <nik-ves@hotmail.com>
2021-11-30 12:21:55 +0100

    Challenge 1

--- script.js
+++ script.js
@@ -175,6 +175,7 @@ console.log(swiss);
 
 /////////////////////////////////////////////////////////////////////
 
+/*
 const lufthansa = {
   airline: "Lufthansa",
   iataCode: "LH",
@@ -243,3 +244,45 @@ const addTaxRate = function (rate) {
 const addVAT2 = addTaxRate(0.23);
 console.log(addVAT2(100));
 console.log(addVAT2(23));
+*/
+
+/////////////////////////////////////////////////////////////////////
+
+const poll = {
+  question: "What is your favourite programming language?",
+  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    // get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join("\n")}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    // register answer
+    typeof answer === "number" &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResaults();
+    this.displayResaults("string");
+  },
+  displayResaults(type = "array") {
+    if (type === "array") {
+      console.log(this.answers);
+    } else if (type === "string") {
+      console.log(`Poll results are: ${this.answers.join(", ")}`);
+    }
+  },
+};
+// poll.registerNewAnswer();
+
+document
+  .querySelector(".poll")
+  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+
+poll.displayResaults.call({ answers: [5, 2, 3] }, "string");
+poll.displayResaults.call({ answers: [1, 5, 3, 9, 6, 1] }, "string");
