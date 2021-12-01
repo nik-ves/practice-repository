49317c6b5343a56b3c2ddf193b2a8bc230c308d9
nik-ves <nik-ves@hotmail.com>
2021-12-01 15:34:47 +0100

    forEach with maps and sets

--- script.js
+++ script.js
@@ -65,12 +65,6 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
-
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
@@ -131,6 +125,9 @@ console.log(arr.at(-1));
 console.log("nikola".at(-1));
 */
 
+/////////////////////////////////////////////////
+
+/*
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // for (const movement of movements) {
@@ -149,4 +146,26 @@ movements.forEach(function (movement, index, array) {
   } else {
     console.log(`Movement ${index + 1}: You withdrew ${Math.abs(movement)}`);
   }
+});
+*/
+
+/////////////////////////////////////////////////
+
+// Map
+const currencies = new Map([
+  ["USD", "United States dollar"],
+  ["EUR", "Euro"],
+  ["GBP", "Pound sterling"],
+]);
+
+currencies.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+// Set
+const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
+console.log(currenciesUnique);
+
+currenciesUnique.forEach(function (value, key, set) {
+  console.log(`${key}: ${value}`); // key is the same as value
 });
