9a3b376fcd194eed7ecd172ebee203c2d032d380
nik-ves <nik-ves@hotmail.com>
2021-12-02 10:41:08 +0100

    Challenge 3

--- script.js
+++ script.js
@@ -392,6 +392,28 @@ calcAverageHumanAge(kateDogs);
 
 /////////////////////////////////////////////////
 
+// Challenge 3
+const data1 = [5, 2, 4, 1, 15, 8, 3];
+const data2 = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = (ages) => {
+  const humanAges = ages
+    .map((age) => {
+      return age <= 2 ? 2 * age : 16 + age * 4;
+    })
+    .filter((age) => {
+      return age >= 18;
+    })
+    .reduce((acc, curr, i, arr) => {
+      return acc + curr / arr.length;
+    });
+};
+
+calcAverageHumanAge(data1);
+calcAverageHumanAge(data2);
+
+/////////////////////////////////////////////////
+
 /* 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 const eurToUsd = 1.1;
