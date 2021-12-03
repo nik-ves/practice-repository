08b06faf673ff92a0a16a8376849aa529dc1a5d6
nik-ves <nik-ves@hotmail.com>
2021-12-03 10:02:24 +0100

    Numeric separators

--- script.js
+++ script.js
@@ -335,6 +335,7 @@ console.log(+(2.345).toFixed(2));
 
 /////////////////////////////////////////////////
 
+/*
 console.log(5 % 2);
 console.log(5 / 2); // 5 = 2 * 2 + 1
 
@@ -362,5 +363,26 @@ labelBalance.addEventListener("click", () => {
     }
   });
 });
+*/
+
+/////////////////////////////////////////////////
+
+// 287,460,000,000
+const diametar = 287_460_000_000;
+console.log(diametar);
+
+const price = 345_99;
+console.log(price);
+
+const transferFee1 = 15_00;
+const transferFee2 = 1_500;
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number("230000"));
+
+console.log(Number("230000"));
+console.log(parseInt("230_000"));
 
 /////////////////////////////////////////////////
