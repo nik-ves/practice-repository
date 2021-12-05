edf9c88736f0371e7794d81b15902383914096ae
nik-ves <nik-ves@hotmail.com>
2021-12-05 18:58:06 +0100

    DOM Traversing

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+const btnScrollTo = document.querySelector(".btn--scroll-to");
+const section1 = document.querySelector("#section--1");
 
 const openModal = function (event) {
   event.preventDefault();
@@ -32,6 +34,45 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+// Button scrolling
+btnScrollTo.addEventListener("click", () => {
+  section1.scrollIntoView({
+    behavior: "smooth",
+  });
+});
+
+// Page Navigation
+// document.querySelectorAll(".nav__link").forEach((link) => {
+//   link.addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     const id = this.getAttribute("href");
+//     console.log(id);
+
+//     document.querySelector(id).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
+
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+document
+  .querySelector(".nav__links")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+
+    // Matching strategy
+    if (event.target.classList.contains("nav__link")) {
+      const id = event.target.getAttribute("href");
+      console.log(id);
+
+      document.querySelector(id).scrollIntoView({
+        behavior: "smooth",
+      });
+    }
+  });
 
 ///////////////////////////////////////
 
@@ -114,6 +155,7 @@ logo.className = "nikola";
 
 ///////////////////////////////////////
 
+/*
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 
@@ -149,6 +191,7 @@ btnScrollTo.addEventListener("click", (event) => {
     behavior: "smooth",
   });
 });
+*/
 
 ///////////////////////////////////////
 
@@ -174,6 +217,7 @@ h1.onmouseenter = () => {
 
 ///////////////////////////////////////
 
+/* 
 // rgb(255, 255, 255)
 const randomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -206,3 +250,39 @@ document.querySelector(".nav").addEventListener("click", function (e) {
 
   console.log("NAV", e.target, e.currentTarget);
 });
+*/
+
+///////////////////////////////////////
+
+const h1 = document.querySelector("h1");
+
+// Going downwards: child elements
+console.log(h1.querySelectorAll(".highlight"));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = "white";
+h1.lastElementChild.style.color = "red";
+
+// Going upwards: selecting parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest(".header").style.background = "var(--gradient-secondary)";
+
+h1.closest("h1").style.background = "var(--gradient-primary)";
+
+// Goign sideways: siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el !== h1) {
+    el.style.transform = "scale(0.5)";
+  }
+});
+
+///////////////////////////////////////
