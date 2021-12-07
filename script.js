33b915575542f14a9b78f2ae30dd884ff0d4c779
Nikola Veselinović <nik-ves@hotmail.com>
2021-12-07 11:50:14 +0100

    ES6 classes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // Constructor functions and the new operator
 
+/*
 const Person = function (firstName, birthYear) {
   // instance properties this.firstName and this.birthYear
   this.firstName = firstName;
@@ -98,3 +99,40 @@ bmw.brake();
 mercedes.accelerate();
 mercedes.accelerate();
 mercedes.brake();
+*/
+
+/////////////////////////////////////////////////////////
+
+// class expresion
+// const PersonCl = class {};
+
+// class decleration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  // methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hey ${this.firstName}.`);
+  }
+}
+
+const jessica = new PersonCl("Jessica", 1996);
+console.log(jessica);
+jessica.calcAge();
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}.`);
+// };
+
+jessica.greet();
+
+// 1. Classes are NOT hoisted
+// 2. Classes are first-class sitizens
+// 3. Classes are executed in strict mode
