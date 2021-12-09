4580d283a5b5e1cafb86713b69c3b3f19f17a7ec
Nikola Veselinović <nik-ves@hotmail.com>
2021-12-09 11:08:03 +0100

    Consuming promises

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ getCountryData("germany");
 
 ///////////////////////////////////////
 
+/*
 const renderCountry = function (data, className) {
   let html = `
 <article class="country ${className}">
@@ -90,3 +91,52 @@ const getCountryAndNeighbour = function (country) {
 };
 
 getCountryAndNeighbour("usa");
+*/
+
+///////////////////////////////////////
+
+// const request = new XMLHttpRequest();
+// request.open("GET", `https://restcountries.com/v2/name/${country}`);
+// request.send();
+
+const renderCountry = function (data, className) {
+  let html = `
+<article class="country ${className}">
+  <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)}M people.</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+</article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const request = fetch(`https://restcountries.com/v2/name/serbia`);
+console.log(request);
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.com/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0]));
+};
+
+getCountryData("serbia");
