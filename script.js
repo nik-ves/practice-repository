f39b9fef08b494a1bcc2f0ecf534d819543044b3
Nikola Veselinović <nik-ves@hotmail.com>
2021-12-14 00:12:19 +0100

    Top-level await (ES2022)

--- script.js
+++ script.js
@@ -21,3 +21,24 @@ add("pizza", 2);
 add("apples", 4);
 
 console.log(cart);
+
+// const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// const data = await res.json();
+// console.log(data);
+// console.log("Something");
+
+const getLastPost = async function () {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return { title: data.at(-1).title, text: data.at(-1).body };
+};
+
+const lastPost = getLastPost();
+console.log(lastPost);
+
+// not very clean
+lastPost.then((last) => console.log(last));
+
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
