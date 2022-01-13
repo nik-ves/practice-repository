2e8caf8fdc879cf3f4f1c329b6dd5627d4313ce5
Nikola Veselinović <nik-ves@hotmail.com>
2022-01-14 00:31:18 +0100

    Load and hashchange events

--- src/js/controller.js
+++ src/js/controller.js
@@ -30,11 +30,15 @@ const renderSpinner = (parentEl) => {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
 
     const res = await fetch(
-      "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886"
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
       // "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bccbd"
     );
     const data = await res.json();
@@ -163,4 +167,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+["hashchange", "load"].forEach((ev) => window.addEventListener(ev, showRecipe));
