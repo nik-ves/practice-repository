7ee9134e1c0891565e2364ddca6d5e202674ef95
Nikola Veselinović <nik-ves@hotmail.com>
2022-01-19 14:48:28 +0100

    Adding styles with stylesheets

--- App.js
+++ App.js
@@ -3,64 +3,30 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50, flexDirection: "row" }}>
-      <View
-        style={{
-          backgroundColor: "red",
-          width: 100,
-          height: 100,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>1</Text>
-      </View>
-
-      <View
-        style={{
-          backgroundColor: "blue",
-          width: 100,
-          height: 100,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>2</Text>
-      </View>
-
-      <View
-        style={{
-          backgroundColor: "green",
-          width: 100,
-          height: 100,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>3</Text>
-      </View>
-      {/* <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
-        <TextInput
-          placeholder="Course goal"
-          style={{
-            borderColor: "black",
-            borderWidth: 1,
-            padding: 10,
-            width: "80%",
-          }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course goal" style={styles.input} />
 
         <Button title="ADD" />
       </View>
-      <View></View> */}
+      <View></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 30,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+    width: "80%",
+  },
+});
