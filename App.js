8242057225b44247ed6011c1ac080a501fd04e9d
Nikola Veselinović <nik-ves@hotmail.com>
2022-01-19 14:42:56 +0100

    Flexbox

--- App.js
+++ App.js
@@ -3,8 +3,43 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 30 }}>
+    <View style={{ padding: 50, flexDirection: "row" }}>
       <View
+        style={{
+          backgroundColor: "red",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>1</Text>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: "blue",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+
+      <View
+        style={{
+          backgroundColor: "green",
+          width: 100,
+          height: 100,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
+      </View>
+      {/* <View
         style={{
           flexDirection: "row",
           justifyContent: "space-between",
@@ -23,7 +58,7 @@ export default function App() {
 
         <Button title="ADD" />
       </View>
-      <View></View>
+      <View></View> */}
     </View>
   );
 }
