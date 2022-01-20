bf96efa6544b60154adb9672fcda31f41d480cde
Nikola Veselinović <nik-ves@hotmail.com>
2022-01-20 22:59:45 +0100

    More game logic

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -12,6 +12,7 @@ import { useState } from "react/cjs/react.development";
 
 import Card from "../components/Card";
 import Input from "../components/Input";
+import NumberContainer from "../components/NumberContainer";
 import Colors from "../constants/colors";
 
 const StartGameScreen = () => {
@@ -44,12 +45,21 @@ const StartGameScreen = () => {
     setConfirmed(true);
     setSelectedNumber(chosenNumber);
     setEnteredValue("");
+    Keyboard.dismiss();
   };
 
   let confirmedOutput;
 
   if (confirmed) {
-    confirmedOutput = <Text>Chosen Number: {selectedNumber}</Text>;
+    confirmedOutput = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selected</Text>
+
+        <NumberContainer>{selectedNumber}</NumberContainer>
+
+        <Button title="START GAME" />
+      </Card>
+    );
   }
 
   return (
@@ -121,6 +131,10 @@ const styles = StyleSheet.create({
   input: {
     width: 50,
     textAlign: "center",
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: "center",
   },
 });
 
