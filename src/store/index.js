879073d65b33ae74350ddb9320c6c805263786c7
Nikola Veselinović <nik-ves@hotmail.com>
2022-01-21 13:25:10 +0100

    Action payload and multiple state properties

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,34 @@
 // import redux from "redux";
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initalState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initalState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
+
+  if (action.type === "increase") {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
