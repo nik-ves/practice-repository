a34d161d4a00ecceb3926267c4d4f59b9bf63a9a
Nikola Veselinović <nik-ves@hotmail.com>
2022-01-23 16:19:11 +0100

    Finalizing the fetching logic

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -19,7 +19,12 @@ export const fetchCart = () => {
 
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
