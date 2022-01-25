fcdffdb29afbaa53eca68f4ae676ab6831e28618
Nikola Veselinović <nik-ves@hotmail.com>
2022-01-25 12:44:55 +0100

    Custom wrapper components

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -13,6 +13,8 @@ import { useState } from "react/cjs/react.development";
 import Card from "../components/Card";
 import Input from "../components/Input";
 import NumberContainer from "../components/NumberContainer";
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 import Colors from "../constants/colors";
 
 const StartGameScreen = (props) => {
@@ -68,10 +70,10 @@ const StartGameScreen = (props) => {
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <View style={styles.screen}>
-        <Text style={styles.title}>Start a New Game!</Text>
+        <TitleText style={styles.title}>Start a New Game!</TitleText>
 
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <BodyText>Select a Number</BodyText>
 
           <Input
             style={styles.input}
