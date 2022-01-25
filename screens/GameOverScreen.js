4a3350b24be9bb71e419dac278cb6dd5a9afaf42
Nikola Veselinović <nik-ves@hotmail.com>
2022-01-25 12:59:35 +0100

    Adding and styling local images and working with web images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,25 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+
+import BodyText from "../components/BodyText";
+import TitleText from "../components/TitleText";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over!</Text>
-      <Text>Number of rounds: {props.roundsNumber}</Text>
-      <Text>Number was: {props.userNumber}</Text>
+      <TitleText>The Game is Over!</TitleText>
+
+      <View style={styles.imageContainer}>
+        {/* <Image source={require("../assets/success.png")} style={styles.image} /> -- local image */}
+        <Image
+          source={{
+            uri: "https://media.istockphoto.com/photos/ama-dablam-mount-in-the-nepal-himalaya-picture-id485966046?k=20&m=485966046&s=612x612&w=0&h=gxP8DAYg54epuymP-eLMvh4hmlIm-AchRMwwNjzBUwE=",
+          }}
+          style={styles.image}
+        />
+      </View>
+
+      <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
+      <BodyText>Number was: {props.userNumber}</BodyText>
 
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
@@ -17,6 +31,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 
