1072341a92cc88cbe1ab2ad0e62a6275d11e1063
Nikola Veselinović <nik-ves@hotmail.com>
2022-02-23 23:17:11 +0100

    URL parameters

--- App.js
+++ App.js
@@ -19,6 +19,24 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+app.get("/api/v1/tours/:id", (req, res) => {
+  console.log(req.params);
+
+  if (req.params.id > tours.length) {
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid ID!",
+    });
+  } else {
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour: tours.find((tour) => tour.id === +req.params.id),
+      },
+    });
+  }
+});
+
 app.post("/api/v1/tours", (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
