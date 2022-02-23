066fda52ea3653d29fef18edbc31c9e9d84e25fe
Nikola Veselinović <nik-ves@hotmail.com>
2022-02-23 23:06:02 +0100

    Handling POST requests

--- App.js
+++ App.js
@@ -3,13 +3,7 @@ const fs = require("fs");
 
 const app = express();
 
-// app.get("/", (req, res) => {
-//   res.status(200).json({ message: "Ovo je neki test", app: "Nikola" });
-// });
-
-// app.post("/", (req, res) => {
-//   res.send("You can post to this endpoint!");
-// });
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -23,6 +17,26 @@ app.get("/api/v1/tours", (req, res) => {
       tours,
     },
   });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: "success",
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
