bff03d2116c0bec8989d2fdd894785ca5550e5ce
Nikola Veselinović <nik-ves@hotmail.com>
2022-03-10 23:21:51 +0100

    Refactoring API features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,58 +1,22 @@
 const Tour = require("./../models/tourModel");
+const APIFeatures = require("../utils/apiFeatures");
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = "5";
-  req.query.sort = "-ratingsdAverage,price";
+  req.query.sort = "-ratingsAverage,price";
   req.query.fields = "name,price,ratingsAverage,summary,difficulty";
   next();
 };
 
 exports.getAllTours = async (req, res) => {
   try {
-    // Filtering
-    const queryObj = { ...req.query };
-    const excludeFields = ["page", "sort", "limit", "fields"];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    // Advanced filtering
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // Sorting
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(",").join(" ");
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort("-createdAt");
-    }
-
-    // Field limiting
-    if (req.query.fields) {
-      const fields = req.query.fields.split(",").join(" ");
-      query = query.select(fields);
-    } else {
-      query = query.select("-__v");
-    }
-
-    // PAGINATION
-    const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-    // page=2&limit=10, 1-10, page 1, 11-20, page 2
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const numTours = await Tour.countDocuments();
-
-      if (skip >= numTours) {
-        throw new Error("This page does not exist!");
-      }
-    }
-
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
 
     res.status(200).json({
       status: "success",
