51c5107d2b0d5073b9cbd6effcb5a0091c3cd544
Nikola Veselinović <nik-ves@hotmail.com>
2022-03-17 00:47:55 +0100

    Updating and deleting documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -55,32 +55,39 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {
-  if (+req.params.id > tours.length) {
-    return res.status(404).json({
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
       status: "fail",
-      message: "Invalid ID.",
+      message: error.message,
     });
   }
-
-  res.status(200).json({
-    status: "success",
-    data: {
-      tour: "<Updated tour here...>",
-    },
-  });
 };
 
-exports.deleteTour = (req, res) => {
-  if (+req.params.id > tours.length) {
-    return res.status(404).json({
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
       status: "fail",
-      message: "Invalid ID.",
+      message: error.message,
     });
   }
-
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
 };
