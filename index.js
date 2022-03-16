3950b27c9c1f6041b012632c1e91b7759b5b7412
Nikola Veselinović <nik-ves@hotmail.com>
2022-03-16 21:53:13 +0100

    GET and POST requests

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 require("dotenv").config();
 const express = require("express");
+const fs = require("fs");
 
 const app = express();
 
+// for us to be able to send data with post request this middleware needs to be here
 app.use(express.json());
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  console.log(req.body);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      tour: req.body,
+    },
+  });
+});
 
 const PORT = process.env.PORT || 3000;
 
