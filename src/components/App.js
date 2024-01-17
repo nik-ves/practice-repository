46b9b40d8f1f981266d43d915725c6a1d916e247
Nikola Veselinović <nik-ves@hotmail.com>
2024-01-17 23:53:45 +0100

    S07 - 85 Derived state

--- src/components/App.js
+++ src/components/App.js
@@ -31,7 +31,7 @@ export default function App() {
         onToggleItem={handleToggleItem}
       />
 
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -116,10 +116,24 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">Start adding some items to your packing list!</p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go."
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} / (${percentage}%)`}
+      </em>
     </footer>
   );
 }
