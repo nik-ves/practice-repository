c8d001be1a06ef27124d5e14460d5f0b8a8ebc45
Nikola Veselinović <nik-ves@hotmail.com>
2024-01-17 23:38:11 +0100

    S07 - 82 Deleting the items

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,5 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "Charger", quantity: 1, packed: false },
-];
-
 export default function App() {
   const [items, setItems] = useState([]);
 
@@ -14,11 +8,14 @@ export default function App() {
     console.log(items);
   }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -67,15 +64,26 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} />
+          <Item item={item} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
+  );
+}
+
+function Item({ item, onDeleteItem }) {
+  return (
+    <li key={item.id}>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
+    </li>
   );
 }
 
@@ -84,16 +92,5 @@ function Stats() {
     <footer className="stats">
       <em>You have X items on your list, and you already packed X</em>
     </footer>
-  );
-}
-
-function Item({ item }) {
-  return (
-    <li key={item.id}>
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity} {item.description}
-      </span>
-      <button>❌</button>
-    </li>
   );
 }
