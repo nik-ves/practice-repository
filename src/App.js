de4807ad514324217e22e52fa00b2ea63849d936
Nikola Veselinović <nik-ves@hotmail.com>
2024-02-20 20:55:45 +0100

    S12 - 145 Using an async function

--- src/App.js
+++ src/App.js
@@ -201,11 +201,19 @@ const KEY = "f338fc6a";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const query = "interstellar";
 
   useEffect(function () {
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`).then(
-      (response) => response.json().then((data) => setMovies(data.Search))
-    );
+    async function fetchMovies() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+      );
+      const data = await res.json();
+
+      setMovies(data.Search);
+    }
+
+    fetchMovies();
   }, []);
 
   return (
