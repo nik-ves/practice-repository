ae92855200c99846f03e9b9160d9808987dd54c6
Nikola Veselinović <nik-ves@hotmail.com>
2024-02-23 23:20:16 +0100

    S16 - 191 React quiz app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import DateCounter from "./DateCounter";
+import { useEffect } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import { useReducer } from "react";
+
+const initalState = {
+  questions: [],
+  // loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Unknown function.");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initalState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json)
+      .then((data) => dispatch({ tpye: "dataReceived", payload: data }))
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
     </div>
   );
 }
