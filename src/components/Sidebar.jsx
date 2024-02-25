ba5d68ac98956a618c1ed8d895bb5cc109b99ec4
Nikola Veselinović <nik-ves@hotmail.com>
2024-02-25 21:26:32 +0100

    S17 - 212 Nested routes and index route

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
 
 export default function Sidebar() {
   return (
@@ -8,7 +9,8 @@ export default function Sidebar() {
       <Logo />
       <AppNav />
 
-      <p>List of cities</p>
+      <Outlet />
+
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
