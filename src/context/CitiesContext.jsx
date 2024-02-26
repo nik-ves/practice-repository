f15c3b6429d347ac647081285fc72b8f1135a52d
Nikola Veselinović <nik-ves@hotmail.com>
2024-02-26 23:34:47 +0100

    S18 - 227 WorldWise - CitiesContext

---
+++ src/context/CitiesContext.jsx
@@ -0,0 +1,45 @@
+import { useContext, useEffect, useState } from "react";
+import { createContext } from "vm";
+
+const BASE_URL = "http://localhost:8000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState({});
+
+  useEffectt(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+
+        setCities(data);
+      } catch (error) {
+        alert("There was an error loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+
+  if (context === undefined) throw new Error("You cant use context here");
+
+  return context;
+}
+
+export { CitiesProvider, useCities };
