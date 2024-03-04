183d605b901737f693a9b762209405f4c0055491
Nikola Veselinović <nik-ves@hotmail.com>
2024-03-04 14:48:03 +0100

    S19 - 255 Workout timer starter files
    Workout timer app

--- src/App.js
+++ src/App.js
@@ -1,180 +1,63 @@
 import { useEffect, useState } from "react";
-import { faker } from "@faker-js/faker";
-import { PostProvider, usePosts } from "./PostContext";
-
-function createRandomPost() {
-  return {
-    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
-    body: faker.hacker.phrase(),
-  };
-}
+import Calculator from "./Calculator";
+import ToggleSounds from "./ToggleSounds";
 
 function App() {
-  const [isFakeDark, setIsFakeDark] = useState(false);
+  const [allowSound, setAllowSound] = useState(true);
+  const [time, setTime] = useState(formatTime(new Date()));
 
-  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
-  useEffect(
-    function () {
-      document.documentElement.classList.toggle("fake-dark-mode");
+  // Will be be AM or PM
+  const partOfDay = time.slice(-2);
+
+  const workouts = [
+    {
+      name: "Full-body workout",
+      numExercises: partOfDay === "AM" ? 9 : 8,
     },
-    [isFakeDark]
-  );
+    {
+      name: "Arms + Legs",
+      numExercises: 6,
+    },
+    {
+      name: "Arms only",
+      numExercises: 3,
+    },
+    {
+      name: "Legs only",
+      numExercises: 4,
+    },
+    {
+      name: "Core only",
+      numExercises: partOfDay === "AM" ? 5 : 4,
+    },
+  ];
 
-  return (
-    <PostProvider>
-      <section>
-        <button
-          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-          className="btn-fake-dark-mode"
-        >
-          {isFakeDark ? "☀️" : "🌙"}
-        </button>
+  function formatTime(date) {
+    return new Intl.DateTimeFormat("en", {
+      month: "short",
+      year: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(date);
+  }
 
-        <Header />
-        <Main />
-        <Archive />
-        <Footer />
-      </section>
-    </PostProvider>
-  );
-}
+  useEffect(function () {
+    const id = setInterval(function () {
+      setTime(formatTime(new Date()));
+    }, 1000);
 
-function Header() {
-  // 3) Consuming the context value
-  const { onClearPosts } = usePosts();
+    return () => clearInterval(id);
+  }, []);
 
-  return (
-    <header>
-      <h1>
-        <span>⚛️</span>The Atomic Blog
-      </h1>
-      <div>
-        <Results />
-        <SearchPosts />
-        <button onClick={onClearPosts}>Clear posts</button>
-      </div>
-    </header>
-  );
-}
-
-function SearchPosts() {
-  const { searchQuery, setSearchQuery } = usePosts();
-
-  return (
-    <input
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-      placeholder="Search posts..."
-    />
-  );
-}
-
-function Results() {
-  const { posts } = usePosts();
-
-  return <p>🚀 {posts.length} atomic posts found</p>;
-}
-
-function Main() {
   return (
     <main>
-      <FormAddPost />
-      <Posts />
+      <h1>Workout timer</h1>
+      <time>For your workout on {time}</time>
+      <ToggleSounds allowSound={allowSound} setAllowSound={setAllowSound} />
+      <Calculator workouts={workouts} allowSound={allowSound} />
     </main>
   );
-}
-
-function Posts() {
-  return (
-    <section>
-      <List />
-    </section>
-  );
-}
-
-function FormAddPost() {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-
-  const { onAddPost } = usePosts();
-
-  const handleSubmit = function (e) {
-    e.preventDefault();
-    if (!body || !title) return;
-    onAddPost({ title, body });
-    setTitle("");
-    setBody("");
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Post title"
-      />
-      <textarea
-        value={body}
-        onChange={(e) => setBody(e.target.value)}
-        placeholder="Post body"
-      />
-      <button>Add post</button>
-    </form>
-  );
-}
-
-function List() {
-  const { posts } = usePosts();
-
-  return (
-    <ul>
-      {posts.map((post, i) => (
-        <li key={i}>
-          <h3>{post.title}</h3>
-          <p>{post.body}</p>
-        </li>
-      ))}
-
-      {/* <Test /> */}
-    </ul>
-  );
-}
-
-function Archive() {
-  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
-  const [posts] = useState(() =>
-    // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
-    Array.from({ length: 10000 }, () => createRandomPost())
-  );
-
-  const [showArchive, setShowArchive] = useState(false);
-  const { onAddPost } = usePosts();
-
-  return (
-    <aside>
-      <h2>Post archive</h2>
-      <button onClick={() => setShowArchive((s) => !s)}>
-        {showArchive ? "Hide archive posts" : "Show archive posts"}
-      </button>
-
-      {showArchive && (
-        <ul>
-          {posts.map((post, i) => (
-            <li key={i}>
-              <p>
-                <strong>{post.title}:</strong> {post.body}
-              </p>
-              <button onClick={() => onAddPost(post)}>Add as new post</button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </aside>
-  );
-}
-
-function Footer() {
-  return <footer>&copy; by The Atomic Blog ✌️</footer>;
 }
 
 export default App;
