8ec746a98adbcf6c4efa8b252164da4cec296d07
Nikola Veselinović <nik-ves@hotmail.com>
2024-03-05 10:22:58 +0100

    S20 - 271 Making an API call with redux thunks

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
@@ -7,27 +8,6 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
-
-// store.dispatch({ type: "account/deposit", payload: 500 });
-// console.log(store.getState());
-
-// store.dispatch({
-//   type: "account/requestLoan",
-//   payload: { amount: 1000, purpose: "Buy a car" },
-// });
-
-// console.log(store.getState());
-
-// store.dispatch({ type: "account/payLoan" });
-
-// console.log(store.getState());
-
-// store.dispatch(deposit(500));
-// store.dispatch(withdraw(200));
-// console.log(store.getState());
-
-// store.dispatch(createCustomer("Nikola Veselinovic", "123456"));
-// console.log(store.getState());
