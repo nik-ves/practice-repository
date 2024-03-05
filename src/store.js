bfe1b0e351f7e248e9d437f434a7b322ae4e2a47
Nikola Veselinović <nik-ves@hotmail.com>
2024-03-05 09:20:20 +0100

    S20 - 264 Working with action creators

--- src/store.js
+++ src/store.js
@@ -38,16 +38,36 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({ type: "account/deposit", payload: 500 });
-console.log(store.getState());
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
 
-console.log(store.getState());
+// console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+// store.dispatch({ type: "account/payLoan" });
 
+// console.log(store.getState());
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount: amount, purpose: purpose },
+  };
+}
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
 console.log(store.getState());
