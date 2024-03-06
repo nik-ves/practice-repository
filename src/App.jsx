26afa45f78d07dfc02ca914e70a5dcdfc574bb81
Nikola Veselinović <nik-ves@hotmail.com>
2024-03-06 19:25:13 +0100

    S22 - 289 Handling errors with error elements

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +20,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
