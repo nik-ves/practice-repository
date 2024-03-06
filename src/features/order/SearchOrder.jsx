85bb6f765f0125b97a62602bdd5cc2e2172bbbb8
Nikola Veselinović <nik-ves@hotmail.com>
2024-03-06 19:35:08 +0100

    S22 - 290 Fetching orders

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(event) => {
+          setQuery(event.target.value);
+        }}
+      />
+    </form>
+  );
+}
