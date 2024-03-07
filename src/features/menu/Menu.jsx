fa2d412f865cb347942ee40416ad28b54874c4f3
Nikola Veselinović <nik-ves@hotmail.com>
2024-03-07 20:27:57 +0100

    S24 - 312 Init files

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,12 +1,12 @@
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
   const menu = useLoaderData();
 
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
@@ -16,7 +16,6 @@ function Menu() {
 
 export async function loader() {
   const menu = await getMenu();
-
   return menu;
 }
 
