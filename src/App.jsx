ea2f8737209bc4f7298f099261c2df493eb863bb
Nikola Veselinović <nik-ves@hotmail.com>
2024-03-13 14:41:04 +0100

    S25 - 330 Introduction to styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,39 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <StyledApp>
+      <H1>The wild Oasis</H1>
+
+      <Button>Test</Button>
+
+      <Input type="number" placeholder="Number of guests" />
+    </StyledApp>
+  );
 }
